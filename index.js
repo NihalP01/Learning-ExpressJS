5c7f4f85f8594f468f33ca8ea509e2f6be8de7b5
NihalP01 <nihalburagohain02@gmail.com>
2021-05-09 15:52:29 +0530

    Fixed body parser

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express()
 const bodyParser = require('body-parser')
 
-//Student api route
-app.use('/api/students', require('./routes/api/students'));
-
 //Body parser Middleware
-app.use(express.json);
+app.use(express.json());
 app.use(express.urlencoded({ extended: false })); 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+//Student api route
+app.use('/api/students', require('./routes/api/students'));
 
 
 
